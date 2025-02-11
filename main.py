b163a29a853cac0b6efde95f6785476a7b7d164e
Nickolatte <nicholashychuah@gmail.com>
2025-02-11 15:03:18 +0000

    no idea
    random stuff atp

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import tkinter as tk
 from tkinter import PhotoImage
+import pandas as  pd
+
+
 window =tk.Tk(className=" Nicholas' Pokedex")
-window.geometry("900x675")
-backgroundimage = PhotoImage(file="pokedexbackground.png")
+window.geometry("910x910")
+backgroundimage = PhotoImage(file="./pokedexbackground.png")
 image_label = tk.Label(window, image=backgroundimage)
-image_label.place(x=0,y=0)
-login_lbl = tk.Label(text="Login", bg="#ee5351", fg="white" ,width="20")
-login_lbl.grid(row=0, column=0,sticky="nsew", padx=10, pady=10 )
-name_lbl =tk.Label(text="Name", bg="#ee5351", fg="white" ,width="20")
-name_lbl.grid(row=0, column=1,sticky="nsew", padx=10, pady= 10)
-name_ent =tk.Entry()
-name_ent.grid(row=0, column=2,sticky="nsew", padx=10, pady= 10)
-password_lbl = tk.Label(text="Password", bg="#ee5351", fg="white" ,width="20")
-password_lbl.grid(row=1, column=1,sticky="nsew", padx=10, pady= 10)
-password_ent = tk.Entry()
-password_ent.grid(row=1, column=2,sticky="nsew", padx=10, pady= 10)
+image_label.place()
+
+frm_reg_or_log = tk.Frame(window, width=910, height=910)
+
+btn_register= tk.Button(frm_reg_or_log, text="Register", fg="Black",height=6,width=12,  )
+btn_register.place(x=265, y=135)
+frm_reg_or_log.pack()
+
+
+
+
+
+
 window.mainloop()
